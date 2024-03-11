45ff8c538686788a23e1177ab02f368fb09e6155
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-03-11 11:44:34 +0100

    Added Size-enum for container, and new classes that matches the new requirements.

---
+++ Cdull.V2024.HarborSimulation.SimulationFramework/Truck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cdull.V2024.HarborSimulation.SimulationFramework
+{
+    internal class Truck
+    {
+        int id;
+        public int Id { get { return id; } }
+
+        
+    }
+}
