6b9a4b4752c8c360e6bd3de3f92bd1ca2eb883d2
andersym01 <anders.ym@hotmail.no>
2024-03-26 17:08:47 +0100

    xml commetns

--- Cdull.V2024.HarborSimulation.SimulationFramework/Column.cs
+++ Cdull.V2024.HarborSimulation.SimulationFramework/Column.cs
@@ -4,6 +4,9 @@ using System.Linq;
 
 namespace Cdull.V2024.HarborSimulation.SimulationFramework
 {
+    /// <summary>
+    /// Represents a column used for storage in the harbor.
+    /// </summary>
     public class Column
     {
         private int id;
@@ -12,8 +15,21 @@ namespace Cdull.V2024.HarborSimulation.SimulationFramework
         private int distance;
         private int length;
 
+
+        /// <summary>
+        /// List containing the elements of the column.
+        /// </summary>
         public List<int> ColumnList { get; set; }
 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Column"/> class with specified parameters.
+        /// </summary>
+        /// <param name="columnId">The unique identifier of the column.</param>
+        /// <param name="columnHeight">The height of the column.</param>
+        /// <param name="columnWidth">The width of the column.</param>
+        /// <param name="columnDistance">The distance of the column.</param>
+        /// <param name="distance">The distance of the column.</param>
         public Column(int columnId, int columnHeight, int columnWidth, int columnDistance, int distance)
         {
             id = columnId;
@@ -25,6 +41,13 @@ namespace Cdull.V2024.HarborSimulation.SimulationFramework
             ColumnList = Enumerable.Repeat(0, columnHeight * columnWidth * columnDistance).ToList();
         }
 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Column"/> class with specified parameters.
+        /// </summary>
+        /// <param name="id">The unique identifier of the column.</param>
+        /// <param name="length">The length of the column.</param>
+        /// <param name="height">The height of the column.</param>
         public Column(int id, int length, int height)
         {
             this.id = id;
